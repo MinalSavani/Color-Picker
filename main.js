72d52a187f229b0cab3719788d1c5a34325db7cf
bhargav-tibadiya <bhargavptibadiya.tbp@gmail.com>
2024-08-10 20:47:10 +0530

    Design improved and some Code improvement done

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-document.querySelectorAll('input[type=color]').forEach(function(picker) {
-    var targetLabel = document.querySelector('label[for="' + picker.id + '"]');
-    var colorArea = document.createElement('span');
-    
-    colorArea.innerHTML = picker.value;
-    targetLabel.appendChild(colorArea);
+document.querySelectorAll('input[type=color]').forEach(function (picker) {
+  var targetLabel = document.querySelector('label[for="' + picker.id + '"]');
+  var colorArea = document.createElement('span');
 
-    picker.addEventListener('change', function() {
-        colorArea.innerHTML = picker.value;
-    });
+  const container = document.getElementById('container')
+
+  colorArea.innerHTML = picker.value;
+  console.log('picker.value', picker.value)
+  targetLabel.appendChild(colorArea);
+
+  picker.addEventListener('change', function () {
+    colorArea.innerHTML = picker.value.toUpperCase();
+    container.style.backgroundColor = picker.value;
+  });
 });
